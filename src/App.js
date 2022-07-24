f699cf95d41b4ddd492eb1631fbb798dc72df374
marieleponti <marieleponti@gmail.com>
2022-07-24 13:53:41 -0500

    multiple components, added images dynamically

--- src/App.js
+++ src/App.js
@@ -6,7 +6,34 @@ function App() {
   return (
     <div className="App">
       <h1>KIDS BOOKS TO CHECK OUT</h1>
-      <Review />
+      <Review 
+        bookTitle="Ahmad Aziz's Epic Year"
+        image='AhmedAzizsEpicYear'
+        author='Nina Hamza'
+        synopsis='A Indian American boy endures a family move from Hawaii to frigid 
+        Minnesota and, with the help of three life-changing books he reads in school, he learns to 
+        like reading, and ultimately, himself.'
+        reviewText="From School Library Journal: &quot;A strong debut destined to become 
+        a classic. Recommended for classrooms and libraries of all types, particularly where Zanib 
+        Mian's &quot;Planet Omar&quot; series flies off the shelves.&quot; --Mahasin A. Aleem, Oakland P.L., CA"
+      />
+       <Review 
+        bookTitle="A Sky-Blue Bench"
+        image='aSkyBlueBench'
+        author='Bahram Rahman'
+        synopsis='Young Aria returns to school after recovering from an accident and being fitted with 
+        a prosthetic leg, but the school has no furniture and sitting on the floor is too painful. 
+        She finds a way to build her own bench, surprising and inspiring her classmates.'
+        reviewText=""
+      />
+        <Review 
+        bookTitle="I Love Vincent"
+        image='iLoveVincent'
+        author='Laura Ljungkvist'
+        synopsis='Scout, a seeing-eye dog, and his human, Vincent, who is blind, make their way 
+        through a typical day.'
+        reviewText=""
+      />
     </div>
   );
 }
--- src/components/Review.js
+++ src/components/Review.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import '../stylesheets/Review.css'
 
-function Review(){
+function Review(props){
   return (
     <div className='review-container'>
       <img 
         className='review-image'
-        src={require('../images/bookcover_AhmedAzizsEpicYear.png')}
+        src={require(`../images/bookcover_${props.image}.png`)}
         alt='First Review Ahmed Azizs Epic Year'
       />
       <div className='text-review-container'>
-        <p className='book-title'>Ahmad Aziz's Epic Year</p>
-        <p className='author'>Nina Hamza</p>
-        <p className='synopsis'>"A Indian American boy endures a family move from Hawaii to frigid 
-        Minnesota and, with the help of three life-changing books he reads in school, he learns to 
-        like reading, and ultimately, himself."</p>
-        <p className='review-text'>From Schol Library Journal: "A strong debut destined to become 
-        a classic. Recommended for classrooms and libraries of all types, particularly where Zanib 
-        Mian's "Planet Omar" series flies off the shelves.--Mahasin A. Aleem, Oakland P.L., CA"</p>
+        <p className='book-title'>
+          <strong>{props.bookTitle}</strong>
+          </p>
+        <p className='author'>{props.author}</p>
+        <p className='synopsis'>{props.synopsis}</p>
+        <p className='review-text'>{props.reviewText}</p>
       </div>
     </div>
   );
